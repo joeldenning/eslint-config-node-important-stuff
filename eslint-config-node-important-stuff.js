71a130b462fd5ac1f3de53d8015fc1813a634f2e
Joel Denning <joeldenning@gmail.com>
2021-10-04 15:39:04 -0600

    Remove unsupported features lint rule. Add prettier

--- eslint-config-node-important-stuff.js
+++ eslint-config-node-important-stuff.js
@@ -1,10 +1,9 @@
 module.exports = {
-  extends: [
-    "important-stuff",
-    "plugin:node/recommended"
-  ],
+  extends: ["important-stuff", "plugin:node/recommended"],
   rules: {
     "no-console": "off",
-    "no-process-exit": "off"
-  }
-}
+    "no-process-exit": "off",
+    // Doesn't seem to accurately detect which features are supported or which node version is being used
+    "node/no-unsupported-features/es-syntax": "off",
+  },
+};
